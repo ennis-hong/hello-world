f3f2e35a2d3277b1ab7664dba649d21602acfd48
EnnisHong <kugi7821@gmail.com>
2024-02-13 22:05:37 +0800

    feat: edit views add cover and file input

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module BooksHelper
+  def show_cover(book, size)
+    return image_tag book.cover.variant(resize_to_limit: [size, size]) if book.cover.attached?
+    image_tag 'default_img.webp', class: 'img_thumb'
+  end
 end
