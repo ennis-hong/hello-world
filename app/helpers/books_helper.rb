f43bc49fbe89f5341a33f58f3ceee41b2b9f611d
EnnisHong <kugi7821@gmail.com>
2024-02-16 16:00:01 +0800

    chore: add bootstrap and modify css

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 module BooksHelper
+  def show_wrap(target)
+    target.split("\n").join("<br>").html_safe
+  end
+
   def show_cover(book, size)
-    return image_tag book.cover.variant(resize_to_limit: [size, size]) if book.cover.attached?
+    return image_tag book.cover.variant(resize_to_limit: [size, size]),class:'rounded mx-auto d-block' if book.cover.attached?
     image_tag 'default_img.webp', class: 'img_thumb'
   end
 end
